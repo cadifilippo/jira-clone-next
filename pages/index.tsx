d5c400c3613ffdee3c8e1f946dc10e85f1f53d89
Carlos Difilippo <cadifilippo@gmail.com>
2022-04-12 21:59:56 -0300

    add grid and columns

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,28 @@
-import { Typography } from '@mui/material';
+import { Card, CardContent, CardHeader, Grid } from '@mui/material';
 import type { NextPage } from 'next';
 import { Layout } from '../components/layouts';
 
 const Home: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant="h1" color="primary">
-        Hola Jira
-      </Typography>
+    <Layout title="Home | Jira Clone">
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px) ' }}>
+            <CardHeader title="Pendientes" />
+            <CardContent></CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px) ' }}>
+            <CardHeader title="En Progreso" />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px) ' }}>
+            <CardHeader title="Completada" />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
